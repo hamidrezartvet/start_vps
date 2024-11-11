702dbc9e5073233ab6152eb0bd75ba531e6864d3
hamidrezartvet <hamidrezartvet@gmail.com>
2024-11-11 19:05:31 +0330

    true

--- index.sh
+++ index.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-apt update && sudo apt upgrade -y
+sudo apt update && sudo apt upgrade -y
 
+#here we install apache2
+sudo apt install apache2
+
+#then we change apache2 folder access
+sudo chmod -R 755 /var/www
+sudo mkdir -p /var/www/your_domain_1/public_html
+sudo chown -R $USER:$USER /var/www/your_domain_1/public_html
+
+#here we restart apache service
+sudo systemctl reload apache2
+sudo systemctl restart apache2
+
+#here we get apache2 status
+sudo systemctl status apache2
