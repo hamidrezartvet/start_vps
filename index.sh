c9dab9eadd6dc8a3cba93d333ebb8813e2ab6f17
hamidrezartvet <hamidrezartvet@gmail.com>
2024-11-11 18:37:19 +0330

    new_test

--- index.sh
+++ index.sh
@@ -1,6 +1,1 @@
-
-sshpass -p "H@midrezart1vet" ssh -o "StrictHostKeyChecking=no" root@194.26.232.71 "sudo touch true.php && \
- secondLine=$(free -g | sed -n '2p') && \
- ls
-"
-
+sudo apt update && sudo apt upgrade -y
