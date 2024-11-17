c62e1dfad41fbc24d22be8abeea7b48319e0a8d0
hamidrezartvet <hamidrezartvet@gmail.com>
2024-11-17 21:20:29 +0330

    bbr_check

--- index.sh
+++ index.sh
@@ -44,15 +44,21 @@
 # echo 'ip list blocked!';
 
 #here we set bbr for data performance
-net.ipv4.tcp_window_scaling = 1
-net.core.rmem_max = 16777216
-net.core.wmem_max = 16777216
-net.ipv4.tcp_rmem = 4096 87380 16777216
-net.ipv4.tcp_wmem = 4096 16384 16777216
-net.ipv4.tcp_low_latency = 1
-net.ipv4.tcp_slow_start_after_idle = 0
-net.core.default_qdisc = fq
-net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
+echo 'net.ipv4.tcp_window_scaling = 1' >> /etc/sysctl.conf
+echo 'net.core.rmem_max = 16777216' >> /etc/sysctl.conf
+echo 'net.core.wmem_max = 16777216' >> /etc/sysctl.conf
+echo 'net.ipv4.tcp_rmem = 4096 87380 16777216' >> /etc/sysctl.conf
+echo 'net.ipv4.tcp_wmem = 4096 16384 16777216' >> /etc/sysctl.conf
+echo 'net.ipv4.tcp_low_latency = 1' >> /etc/sysctl.conf
+echo 'net.ipv4.tcp_slow_start_after_idle = 0' >> /etc/sysctl.conf
+echo 'net.core.default_qdisc = fq' >> /etc/sysctl.conf
+echo 'net.ipv4.tcp_congestion_control = bbr' >> /etc/sysctl.conf
+sysctl -p
+sysctl net.ipv4.tcp_congestion_control
+
+
+
+
 
 # #at the end we reboot server
 # sudo reboot
