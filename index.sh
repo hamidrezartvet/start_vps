41bcbb876d863fd7d54b945994c2c3c81da19586
hamidrezartvet <hamidrezartvet@gmail.com>
2024-11-17 22:54:22 +0330

    test

--- index.sh
+++ index.sh
@@ -1,33 +1,32 @@
-# #!/bin/bash
-# sudo apt update && sudo apt upgrade -y
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
 
-# #here we install apache2
-# sudo apt install apache2 -y
+#here we install apache2
+sudo apt install apache2 -y
 
-# #then we change apache2 folder access
-# sudo chmod -R 755 /var/www
-# sudo mkdir -p /var/www/your_domain_1/public_html
-# sudo chown -R $USER:$USER /var/www/your_domain_1/public_html
+#then we change apache2 folder access
+sudo chmod -R 755 /var/www
+sudo mkdir -p /var/www/your_domain_1/public_html
+sudo chown -R $USER:$USER /var/www/your_domain_1/public_html
 
-# #here we install php
-# sudo add-apt-repository ppa:ondrej/php
-# sudo apt update
-# sudo apt install php8.2 -y
-# sudo apt-get install -y php8.2-cli php8.2-common php8.2-fpm php8.2-mysql php8.2-zip php8.2-gd php8.2-mbstring php8.2-curl php8.2-xml php8.2-bcmath
-# sudo apt-get install libapache2-mod-php8.2 -y
-# sudo a2enmod php8.2
-# sudo update-alternatives --set php /usr/bin/php8.2 -y
-# sudo service apache2 restart
+#here we install php
+sudo add-apt-repository ppa:ondrej/php
+sudo apt update
+sudo apt install php8.2 -y
+sudo apt-get install -y php8.2-cli php8.2-common php8.2-fpm php8.2-mysql php8.2-zip php8.2-gd php8.2-mbstring php8.2-curl php8.2-xml php8.2-bcmath
+sudo apt-get install libapache2-mod-php8.2 -y
+sudo a2enmod php8.2
+sudo update-alternatives --set php /usr/bin/php8.2 -y
+sudo service apache2 restart
 
-# #install getit and nano
-# sudo apt install nano -y
+#install getit and nano
+sudo apt install nano -y
 
 #download file and put in html folder
 wget  -P /var/www/html "https://raw.githubusercontent.com/hamidrezartvet/start_vps/master/index.php"
 wget  -P /var/www/html "https://raw.githubusercontent.com/hamidrezartvet/start_vps/master/check.php"
 wget  -P /var/www/html "https://raw.githubusercontent.com/hamidrezartvet/start_vps/master/getOnlineUsers.sh"
 wget  -P /var/www/html "https://raw.githubusercontent.com/hamidrezartvet/start_vps/master/i.txt"
-
 
 #here we block iran ip
 sudo apt-get install curl unzip perl xtables-addons-common libtext-csv-xs-perl libmoosex-types-netaddr-ip-perl iptables-persistent -y 
@@ -44,8 +43,6 @@ done
 sudo iptables-save | sudo tee /etc/iptables/rules.v4
 echo 'Iran ip blocked!'
 
-
-
 #here we set bbr for data performance
 sudo echo 'net.ipv4.tcp_window_scaling = 1' >> /etc/sysctl.conf
 sudo echo 'net.core.rmem_max = 16777216' >> /etc/sysctl.conf
@@ -59,8 +56,5 @@ sudo echo 'net.ipv4.tcp_congestion_control = bbr' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo sysctl net.ipv4.tcp_congestion_control
 
-
-
-
 # #at the end we reboot server
-# sudo reboot
+sudo reboot
