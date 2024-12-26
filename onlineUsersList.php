e69eaa49b7ea3741f8bb25211f494bdfdf2af401
hamidrezartvet <hamidrezartvet@gmail.com>
2024-12-26 16:28:43 +0330

    online_check

--- onlineUsersList.php
+++ onlineUsersList.php
@@ -5,23 +5,31 @@
 		exit();
 	}
 
-	//here we define system usage report array
+	// Initialize the result array
 	$system_usage = [];
-
-	// Execute the 'who' command to get logged-in users
-	$output = [];
-	exec("last | grep 'still logged in' | wc -l", $output);
-   
-	// Check if the command executed successfully
+	
+	// Check if the command to get the count executed successfully
 	if ($returnVar !== 0) {
-   
-	 //here we return error message
-	 $system_usage['ONLINE_USERS'] = "Error fetching SSH users.";
-	}else{
-   
-	 //here we return online ssh online users in the system
-	 $system_usage['ONLINE_USERS'] = intval($output[0]);
+		// Return error message if the command fails
+		$system_usage['ONLINE_USERS'] = "Error fetching SSH users.";
+	} else {
+		// Get the total count of online users
+		$system_usage['ONLINE_USERS_COUNT'] = intval($totalOutput[0]);
+	
+		// Execute the 'last' command to fetch the names of logged-in users
+		$userOutput = [];
+		exec("last | grep 'still logged in' | awk '{print $1}' | sort | uniq", $userOutput, $returnVar);
+	
+		if ($returnVar !== 0) {
+			// Return error message if the command fails
+			$system_usage['online_users_list'] = "Error fetching SSH user names.";
+		} else {
+			// Get the list of online SSH user names
+			$system_usage['online_users_list'] = $userOutput;
+		}
 	}
-
+	
+	// Output the result as JSON
 	echo json_encode($system_usage);
+	
 ?>
